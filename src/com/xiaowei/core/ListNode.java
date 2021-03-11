cc6da19d2675bbc30186508623a2b384e385cdf6
wangxw <os-wangxw@jsfund.cn>
2021-03-11 23:02:41 +0800

    两数之和

---
+++ src/com/xiaowei/core/ListNode.java
@@ -0,0 +1,15 @@
+package com.xiaowei.core;
+
+/**
+ * @Author: wangxw
+ * @DateTime: 2021/3/10
+ * @Description: TODO
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/com/xiaowei/list/AddTwoNumbers.java
@@ -0,0 +1,42 @@
+package com.xiaowei.list;
+
+import com.xiaowei.core.ListNode;
+
+/**
+ * @Author: wangxw
+ * @DateTime: 2021/3/10
+ * @Description: 两数相加
+ */
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+
+        return head;
+    }
+}
